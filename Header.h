ba76679188a4e7d6da2ad28b6c7ea7df3ea21be6
Ca5tor <48414173+Ca5tor@users.noreply.github.com>
2021-09-22 08:55:18 +0300

    Header_ex04

---
+++ Header.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef _HEADER_EX_4_H_
+#define _HEADER_EX_4_H_
+
+#include <iostream>
+#include <cctype>
+
+const unsigned int	strsize = 30;
+const unsigned int	size_struct = 5;
+
+struct bop {
+	char			fullname[strsize];
+	char			title[strsize];
+	char			bopname[strsize];
+	unsigned int	preference; // 0 = fullname, 1 = title, 2 = bopname
+};
+
+void Ex4(void);
+
+#endif
